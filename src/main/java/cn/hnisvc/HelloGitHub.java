c0b379b09204bb57c054d79ae0c404d5e1adf4cf
yqlboss <836591188@qq.com>
2018-08-02 15:21:48 +0800

    This is a simple project to say hello github

---
+++ src/main/java/cn/hnisvc/HelloGitHub.java
@@ -0,0 +1,7 @@
+package cn.hnisvc;
+
+public class HelloGitHub {
+	public String sayHello() {
+		return "Hello GitHub";
+	}
+}
---
+++ src/test/java/cn/hnisvc/TestHello.java
@@ -0,0 +1,10 @@
+package cn.hnisvc;
+
+import org.junit.Test;
+
+public class TestHello {
+	@Test
+	public void test() {
+		System.out.println(new HelloGitHub().sayHello());
+	}
+}
